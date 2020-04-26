eb8bcb4d6a7368763dc825df3a42350144e7a0df
avgsteve <steve.leng@gmail.com>
2020-04-26 22:58:18 +0800

    setting up webpack-dev-server, html-webpack-plugin
    1. Setting up different way of initializing webpack-dev-server as localhost:8080 is not working.
    ========
    in package.json "scripts" :
        "start": "webpack-dev-server --mode development --host 0.0.0.0 --hot --open --useLocalIp"
    ========
    (ref:  https://github.com/webpack/webpack-dev-server/issues/1347  )
    
    2. Modify webpack.config.js. Output => path  changed so HtmlWebpackPlugin can work properly
    
    3. Used cli: npm run dev to see if files are output correctly.

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,4 @@
 /*jshint esversion: 6 */
 
 console.log('Imported module');
-export default 23;
+export default 12345;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 /*jshint esversion: 6 */
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // https://webpack.js.org/concepts/
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'), //__dirname refers to current folder where this config.js is located
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'), //__dirname refers to current folder where this config.js is located
+    filename: 'js/bundle.js'
   },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      //讓src folder的html檔案可以自動複製一份新的copy到dist folder裡面
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
