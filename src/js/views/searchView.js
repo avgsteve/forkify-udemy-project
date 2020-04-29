839682f1900d4282c696f1f7852f3d68aa1c5f71
avgsteve <steve.leng@gmail.com>
2020-04-29 23:38:21 +0800

    144. Building the Search View - Part 2 (limit the length of title string) by reduce method
    ref: https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/9939900

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -15,6 +15,28 @@ export const clearInputField = () => elements.searchInput.value = "";
 //clear the results before making new query
 export const clearResults = () => elements.searchResultList.innerHTML = "";
 
+//limit words length and force a line break if too long
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  //if title string array's length is greater than limit. If so, do following
+  if (title.length > limit) {
+    //split the title and put seperately into array then cicle through array with reduce
+    title.split(' ').reduce((acc, current) => {
+      //if accumulated value plus current words' length value is greater than limit
+      if (acc + current.length <= limit) {
+        //then push current item to the newTitle array
+        newTitle.push(current);
+      }
+      //when the acc value is at or greater than limit, the true condition won't be executed and the returned value will be new accumulator
+      return acc + current.length;
+    }, 0);
+
+    // join the words in array return the result as STRING! (shorter than limit of words)
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+}; // this function will be used as callback in template string for variable 'markup' (${limitRecipeTitle(recipe.title)})
+
 const renderRecipe = recipe => {
   //private callback function to be used only by renderResults()
   const markup = `
@@ -24,7 +46,7 @@ const renderRecipe = recipe => {
               <img src="${recipe.image_url}" alt="${recipe.title}">
           </figure>
           <div class="results__data">
-              <h4 class="results__name">${recipe.title}</h4>
+              <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
               <p class="results__author">${recipe.publisher}</p>
           </div>
       </a>
